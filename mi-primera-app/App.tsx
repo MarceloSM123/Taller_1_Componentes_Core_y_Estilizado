7af8402dd9bdc4a8105fb23f2ebe723f2b9edaa8
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-09-10 15:39:18 -0500

    Paso 2: Reutilizar tus Componentes de Lista

--- mi-primera-app/App.tsx
+++ mi-primera-app/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import EncabezadoCorporativo from './src/components/CompanyHeader';
-
+import EmployeeList from './src/components/EmployeeList';
 export default function App() {
   return (
     <View style={styles.container}>
       <EncabezadoCorporativo />
+      <EmployeeList />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
